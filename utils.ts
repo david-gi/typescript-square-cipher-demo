2e17e9138d257eb573e65fae4065360cf5bb808c
David Giagnocavo <47605858+david-gi@users.noreply.github.com>
2022-12-21 10:39:28 +0100

    Update utils.ts
    Chain changed to not throw an error but simply returns original value when funArr is empty.
    Functional composure prevents the need for a try catch for the function flow itself.

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 /// Apply multiple functions is succession
 export function chain<T>(value: T, funArr: { (x: T): T }[]): T {
     switch (funArr.length) {
-        case 0: throw "funArr cannot be empty"
+        case 0: return value
         case 1: return funArr[0](value)
         default: return chain(funArr[0](value), funArr.slice(1))
     }
